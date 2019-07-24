f94ed35e676148ab2ec407579630a3a5d30aeeb6
GandhiGovind <gandhihadoop@gmail.com>
2019-07-25 01:04:05 +0800

    Update SPARKUIConstants.java

--- SPARKUIConstants.java
+++ SPARKUIConstants.java
@@ -2,8 +2,7 @@ package com.test.sparkui;
 class SPARKUIConstants
 {
 
-  public static String USER_NAME="";
-  public static String CRED_KEY="";
+
   public static String LDAP_CERTIFICATION_PATH="";
   public static String PROVIDER_URL="";
   public static String LDAP_GENERIC_ID="";
