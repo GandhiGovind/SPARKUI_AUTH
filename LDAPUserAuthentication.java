17dbc7940b57a029b96304e0004bdf147fafcc31
GandhiGovind <gandhihadoop@gmail.com>
2019-07-25 02:33:43 +0800

    Update LDAPUserAuthentication.java

--- LDAPUserAuthentication.java
+++ LDAPUserAuthentication.java
@@ -1,6 +1,5 @@
 package com.test.sparkui;
 import java.util.Hashtable;
-import java.util.Properties;
 
 import javax.naming.AuthenticationException;
 import javax.naming.CommunicationException;
@@ -10,38 +9,29 @@ import javax.naming.NamingException;
 import javax.naming.ServiceUnavailableException;
 import javax.naming.TimeLimitExceededException;
 import javax.naming.directory.Attributes;
-import javax.naming.directory.InitialDirContext;
 import javax.naming.directory.SearchControls;
 import javax.naming.directory.SearchResult;
 import javax.naming.ldap.InitialLdapContext;
 import javax.naming.ldap.LdapContext;
 
-import org.apache.log4j.Logger;
-
-
-import com.test.util.SPARKUIConstant;
-
 public class LDAPUserAuthentication {
-	Logger log = Logger.getLogger(LDAPUserAuthentication.class);
+	// Logger log = Logger.getLogger(LDAPUserAuthentication.class);
 
 	public String authenticateUser(String username, String password) {
 		String strUrl = "success";
 		String name = "";
-		System.setProperty("javax.net.ssl.trustStore",
-				SPARKUIConstant.LDAP_CERTIFICATION_PATH);
+		System.setProperty("javax.net.ssl.trustStore", SPARKUIConstants.LDAP_CERTIFICATION_PATH);
 		Hashtable env = new Hashtable(11);
 		boolean b = false;
 		int count = 0;
 		boolean responseOk = false;
 		while (count < 5 && !responseOk) {
 			String Securityprinciple = "";
-			env.put(Context.INITIAL_CONTEXT_FACTORY,
-					"com.sun.jndi.ldap.LdapCtxFactory");
-			env.put(Context.PROVIDER_URL, SPARKUIConstant.PROVIDER_URL);
+			env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+			env.put(Context.PROVIDER_URL, SPARKUIConstants.PROVIDER_URL);
 			// env.put(Context.SECURITY_AUTHENTICATION, "simple");
 			// env.put(Context.SECURITY_PRINCIPAL, "uid=<userid>");
-			env.put(Context.SECURITY_PRINCIPAL, "cn=" + username
-					+ ",ou=users,o=<OrgName>");
+			env.put(Context.SECURITY_PRINCIPAL, "cn=" + username + ",ou=users,o=<OrgName>");
 			env.put(Context.SECURITY_CREDENTIALS, password);
 			env.put("com.sun.jndi.ldap.connect.timeout", "8000");
 			try {
@@ -52,8 +42,7 @@ public class LDAPUserAuthentication {
 				b = true;
 				SearchControls searchControls = getSearchControls();
 				name = getUserName(username, ctx, searchControls);
-				System.out.println("name is ldap:" + name + "-"
-						+ String.valueOf(name.length()));
+				System.out.println("name is ldap:" + name + "-" + String.valueOf(name.length()));
 				if (name.isEmpty()) {
 					name = "name:" + username;
 				}
@@ -96,7 +85,7 @@ public class LDAPUserAuthentication {
 					strUrl = "success:" + name.split(":")[1];
 
 				}
-	
+
 				System.out.println(strUrl);
 			}
 			count++;
@@ -104,8 +93,7 @@ public class LDAPUserAuthentication {
 		return strUrl;
 	}
 
-	private String getUserName(String userID, LdapContext ctx,
-			SearchControls searchControls) {
+	private String getUserName(String userID, LdapContext ctx, SearchControls searchControls) {
 		System.out.println("userID: " + userID);
 		String baseDN = "ou=users,o=<org>";
 		String name = "";
@@ -113,9 +101,9 @@ public class LDAPUserAuthentication {
 			NamingEnumeration<SearchResult> answer;
 			answer = ctx.search(baseDN, "cn=" + userID, searchControls);
 
-			if (answer.hasMoreElements()) { 
+			if (answer.hasMoreElements()) {
 				Attributes attrs = answer.next().getAttributes();
-				name = attrs.get("fullName")==null?"":attrs.get("fullName").toString();
+				name = attrs.get("fullName") == null ? "" : attrs.get("fullName").toString();
 			} else {
 				System.out.println("user not found.");
 			}
@@ -134,5 +122,4 @@ public class LDAPUserAuthentication {
 		return cons;
 	}
 
-	
 }
